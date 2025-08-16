287c298452a1b7f41524d621baa3291558a0b71e
itsAboo <mew.thanachot@gmail.com>
2025-08-16 18:17:35 +0700

    fix create button

--- src/components/posts/PostList.tsx
+++ src/components/posts/PostList.tsx
@@ -9,8 +9,8 @@ import {
 } from "../../hooks/access-request";
 import BackdropProgress from "../UI/BackdropProgress";
 import { useGetPostAccesses } from "../../hooks/post-access";
-import { Pagination } from "@mui/material";
-import { useSearchParams } from "react-router-dom";
+import { Button, Pagination } from "@mui/material";
+import { Link, useSearchParams } from "react-router-dom";
 import PostListCardSkeleton from "../skeleton/PostListCardSkeleton";
 
 export default function PostList() {
@@ -37,7 +37,6 @@ export default function PostList() {
     }
   }, [searchParams]);
 
-
   const handleChangePage = (_: React.ChangeEvent<unknown>, value: number) => {
     setSearchParams({ page: String(value) });
     setPage(value);
@@ -62,69 +61,79 @@ export default function PostList() {
     isAccessRequestLoading;
 
   return (
-    <div className="flex flex-col min-h-[calc(100vh-200px)]">
-      <div className="flex-1">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:place-items-center lg:grid-cols-3 gap-2 lg:gap-4 p-2 items-start">
-          {isLoading
-            ? Array.from({ length: 9 }).map((_, i) => (
-                <div key={i} className="w-full sm:max-w-[345px] h-[280px]">
-                  <PostListCardSkeleton />
-                </div>
-              ))
-            : posts?.posts.map((post) => (
-                <div
-                  key={post.id}
-                  className="w-full sm:max-w-[345px] h-[280px]"
-                >
-                  <PostListCard
-                    id={post.id!}
-                    title={post.title}
-                    description={post.description}
-                    author={post.author}
-                    mostWin={post.mostWin}
-                    mostDraw={post.mostDraw}
-                    mostLost={post.mostLost}
-                    updatedAt={post.updatedAt}
-                    access={post.access!}
-                    user={user}
-                    onOpenConfirm={handleOpenConfirm}
-                    postAccess={
-                      postAccesses?.find(
-                        (pa) => pa.postId === post.id && pa.userId === user?.id
-                      ) || null
-                    }
-                    accessRequests={
-                      accessRequest?.filter(
-                        (ac) =>
-                          ac.requesterId === user?.id &&
-                          ac.postId === post.id &&
-                          !user?.posts?.includes(post.id)
-                      ) || null
-                    }
-                  />
-                </div>
-              ))}
+    <>
+      {isLoading && (
+        <div className="flex justify-end items-center">
+          <Button component={Link} to="/create" variant="contained">
+            Create
+          </Button>
         </div>
-      </div>
+      )}
+      <div className="mt-2 sm:mt-3 flex flex-col min-h-[calc(100vh-200px)]">
+        <div className="flex-1">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:place-items-center lg:grid-cols-3 gap-2 lg:gap-4 p-2 items-start">
+            {isLoading
+              ? Array.from({ length: 9 }).map((_, i) => (
+                  <div key={i} className="w-full sm:max-w-[345px] h-[280px]">
+                    <PostListCardSkeleton />
+                  </div>
+                ))
+              : posts?.posts.map((post) => (
+                  <div
+                    key={post.id}
+                    className="w-full sm:max-w-[345px] h-[280px]"
+                  >
+                    <PostListCard
+                      id={post.id!}
+                      title={post.title}
+                      description={post.description}
+                      author={post.author}
+                      mostWin={post.mostWin}
+                      mostDraw={post.mostDraw}
+                      mostLost={post.mostLost}
+                      updatedAt={post.updatedAt}
+                      access={post.access!}
+                      user={user}
+                      onOpenConfirm={handleOpenConfirm}
+                      postAccess={
+                        postAccesses?.find(
+                          (pa) =>
+                            pa.postId === post.id && pa.userId === user?.id
+                        ) || null
+                      }
+                      accessRequests={
+                        accessRequest?.filter(
+                          (ac) =>
+                            ac.requesterId === user?.id &&
+                            ac.postId === post.id &&
+                            !user?.posts?.includes(post.id)
+                        ) || null
+                      }
+                    />
+                  </div>
+                ))}
+          </div>
+        </div>
 
-      <div className="p-4 flex justify-center">
-        <Pagination
-          onChange={handleChangePage}
-          count={posts?.pageCount}
-          page={page}
-          variant="outlined"
-          shape="rounded"
+        <div className="p-4 flex justify-center">
+          <Pagination
+            onChange={handleChangePage}
+            count={posts?.pageCount}
+            page={page}
+            variant="outlined"
+            shape="rounded"
+          />
+        </div>
+
+        <ConfirmModal
+          open={open}
+          setOpen={setOpen}
+          onConfirm={() => handleConfirmSendRequestAccess(selectedPostId!)}
+          title="This Post is private"
+          description="You need permission from the owner to view this post. Send a request to gain access."
         />
+        <BackdropProgress open={isPending} />
       </div>
-
-      <ConfirmModal
-        open={open}
-        setOpen={setOpen}
-        onConfirm={() => handleConfirmSendRequestAccess(selectedPostId!)}
-        title="This Post is private"
-        description="You need permission from the owner to view this post. Send a request to gain access."
-      />
-      <BackdropProgress open={isPending} />
-    </div>
+    </>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,18 +1,9 @@
-import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
 import PostList from "../components/posts/PostList";
 
 export default function Home() {
   return (
     <div>
-      <div className="flex justify-end items-center">
-        <Button component={Link} to="/create" variant="contained">
-          Create
-        </Button>
-      </div>
-      <div className="mt-2 sm:mt-5">
-        <PostList />
-      </div>
+      <PostList />
     </div>
   );
 }
